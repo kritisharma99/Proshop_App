8b1cc092d14e5f408f77d660f47e97a50c92bfd1
kritisharma99 <sharmakriti.k27@gmail.com>
2023-02-22 14:42:44 +0530

    Order Screen Setup

--- frontend/src/screens/OrderScreen.jsx
+++ frontend/src/screens/OrderScreen.jsx
@@ -8,33 +8,36 @@ import Loader from "../components/Loader"
 import { getOrderDetails } from "../actions/orderAction"
 
 function OrderScreen() {
-    let { orderId } = useParams();
-    orderId = String(orderId)
-    console.log("id",orderId)
+    let { id } = useParams();
+    id = String(id)
+    // let { orderId } = useParams();
+    console.log("id",id)
 
     const dispatch = useDispatch()
     const navigate = useNavigate()
     
-    const cart = useSelector((state) => state.cart)
-    
     const orderDetails = useSelector(state => state.orderDetails)
     const { order, loading, error} = orderDetails
-
-    useEffect(()=>{
-        dispatch(getOrderDetails(orderId))
-    })
-    // const placeOrderHandler = () => {
-    //     dispatch(createOrder({
-    //         orderItems: cart.cartItems,
-    //         shippingAddress: cart.shippingAddress,
-    //         paymentMethod: cart.paymentMethod,
-    //         itemsPrice: cart.itemsPrice,
-    //         shippingPrice: cart.shippingPrice,
-    //         taxPrice: cart.taxPrice,
-    //         totalPrice: cart.totalPrice,
-    //     }))
-    //     // console.log('order!')
-    // }
+    console.log(order, loading, error)
+    if (!loading) {
+        //   Calculate prices
+        const addDecimals = (num) => {
+          return (Math.round(num * 100) / 100).toFixed(2)
+        }
+    
+        order.itemsPrice = addDecimals(
+          order.orderItems.reduce((acc, item) => acc + item.price * item.qty, 0)
+        )
+      }
+      useEffect(() => {
+        if(!order || order._id !== id) {
+            dispatch(getOrderDetails(id))
+        }
+    }, [order, id])
+    // useEffect(()=>{
+    //   dispatch(getOrderDetails(id))
+    // },[dispatch,id])
+    
   return loading ? <Loader/>:error ? <Message variant="danger">{error}</Message>
   : <>
   <h1>Order {order._id}</h1>
@@ -44,25 +47,44 @@ function OrderScreen() {
                 <ListGroup.Item>
                     <h2>Shipping</h2>
                     <p>
+                        <strong>Name: </strong> {order.user.name}
+                    </p>
+                    <p>
+                        <strong>Email: </strong>{' '}
+                        <a href={`mailto:${order.user.email}`}>{order.user.email}</a>
+                    </p>
+                    <p>
                         <strong>Address :  </strong>
                         {order.shippingAddress.address},
                         {order.shippingAddress.city} {order.shippingAddress.postalCode}
                         {order.shippingAddress.country}
                     </p>
+                    {order.isDelivered ? (
+                        <Message variant='success'>
+                        Delivered on {order.deliveredAt}
+                        </Message>
+                    ) : (
+                        <Message variant='danger'>Not Delivered</Message>
+                    )}
                 </ListGroup.Item>
 
                 <ListGroup.Item>
-                    <h2>Payment</h2>
-                    <p>
-                        <strong>Payment Method :  </strong>
-                        {order.paymentMethod}
-                    </p>
-                </ListGroup.Item>
+              <h2>Payment Method</h2>
+              <p>
+                <strong>Method: </strong>
+                {order.paymentMethod}
+              </p>
+              {order.isPaid ? (
+                <Message variant='success'>Paid on {order.paidAt}</Message>
+              ) : (
+                <Message variant='danger'>Not Paid</Message>
+              )}
+            </ListGroup.Item>
 
                 <ListGroup.Item>
                     <h2>Order Items</h2>
                     <p>
-                        {cart.orderItems.length === 0 ? <Message> No Orderd Placed!</Message> : (
+                        {order.orderItems.length === 0 ? <Message> No Orderd Placed!</Message> : (
                             <ListGroup variant="flush">
                                 {order.orderItems.map((item,index) => (
                                     <ListGroup.Item key={index}>
@@ -142,11 +164,11 @@ function OrderScreen() {
                         <ListGroupItem>
                             {error && <Message variant ="danger">{error}</Message>}
                         </ListGroupItem>
-                        <ListGroupItem>
+                        {/* <ListGroupItem>
                             {/* <Button type='button' className="btn-block" disabled={cart.cartItems === 0} onClick={placeOrderHandler}>
                                 Place Order
                             </Button> */}
-                        </ListGroupItem>
+                        {/* </ListGroupItem> */}
                   </ListGroup>
               </Card>
           </Col>
